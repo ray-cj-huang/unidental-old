578f474b28f63e18f4090cf5389292b4a4fc66bc
Ray Huang <rayhuang.cj@gmail.com>
2021-09-25 17:28:21 -0700

    setup create-react-app template

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          UNIDENTAL
+        </p>
+        <a
+          className="App-link"
+          href="https://goo.gl/maps/VLhEh3h3rKAzVgkW6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit us!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
