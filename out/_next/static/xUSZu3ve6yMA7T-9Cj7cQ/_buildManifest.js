e897be302b9e074e7c801948ac463a7999be4556
Ray Huang <rayhuang.cj@gmail.com>
2021-10-13 16:49:44 -0700

    Deploy gh-pages

---
+++ out/_next/static/xUSZu3ve6yMA7T-9Cj7cQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/944-70723df56eb49530a410.js","static/chunks/pages/index-595deae9e26027e58cfc.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
